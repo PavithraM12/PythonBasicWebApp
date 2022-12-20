e1ae450fe398e5c0aaf9c176793948e8b9b84036
Pavithra Moravaneni (Student) <pmoravan@sundevils.asu.edu>
2022-12-20 11:38:15 -0700

    HTML changes

--- pythonbasic/wish/urls.py
+++ pythonbasic/wish/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.firstpage),
     path('wish/', views.secondpage),
-    path('wish/pic/', views.thirdpage),
-    path('wish/pic/wish/pic/gift/', views.fourthpage),
+    path('wish/wish/wish/pic', views.thirdpage),
+    path('wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/wish/pic/gift/', views.fourthpage),
 ]
--- pythonbasic/wish/views.py
+++ pythonbasic/wish/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 # from django.http import HttpResponse
 
 
 def firstpage(request):
     return render(request, "firstpage.html")
-
+@csrf_exempt
 def secondpage(request):
     return render(request, "secondpage.html")
-
+# @csrf_exempt
+# def thirdpage(request):
+#     import turtle
+#     t = turtle.Turtle()
+#     turtle.title("Hapiness")
+#     t.right(100)
+#     return t
+@csrf_exempt
 def thirdpage(request):
     return render(request, "thirdpage.html")
 
+@csrf_exempt     
 def fourthpage(request):
     return render(request, "fourthpage.html")
